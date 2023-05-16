233a58ea5a2ac45fb333b2678eb322b122105d4d
Oleg Furculita <Oleg.Furculita1408@gmail.com>
2023-05-16 23:50:26 +0300

    added color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+btnStart.addEventListener('click', onChangeColor);
+btnStop.addEventListener('click', onBtnStop);
+
+let intervalId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
+
+function onChangeColor() {
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        btnStart.disabled = true;
+    }, 1000);
+};
+
+function onBtnStop() {
+    clearInterval(intervalId);
+    btnStart.disabled = false;
+}
